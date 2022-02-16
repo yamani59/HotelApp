27d6ccc7909bab072a58cddffe0b040570238855
Yamani <yamaniyuda@gmail.com>
2022-02-17 06:21:25 +0700

    menghapus link

--- private/views/template/top.php
+++ private/views/template/top.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href=<?= BASEURL . 'css/admin.css' ?>>
-  <script src="https://kit.fontawesome.com/5563521e20.js" crossorigin="anonymous"></script>
+  <!-- <script src="https://kit.fontawesome.com/5563521e20.js" crossorigin="anonymous"></script> -->
   <title>Document</title>
 </head>
 
